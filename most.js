3003d37dcb592c11af59b42c87f14afddfe2f944
destezzykim <destezzykim@gmail.com>
2024-07-13 20:33:58 +0300

    Add files via upload

---
+++ most.js
@@ -0,0 +1,32 @@
+
+var x = 0;
+
+function buttonquotes(){
+if(x ==0 ){
+document.querySelector('.mainshow').style='display:none;';
+x=1;
+}
+else{
+  document.querySelector('.mainshow').style='display:block;'
+  x=0
+}
+}
+
+
+// inpust phone email and message//
+
+
+
+
+
+//last input//
+
+
+function lastinputt (){
+const insend = document.querySelector('jj-input')
+
+const lastin=insend.value;
+insend.value='';
+}
+
+
